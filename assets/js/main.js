26315044f49c9f4313824ee3ae739c87897270e4
Shameel Kadannamanna <shameelsadaka@gmail.com>
2019-12-04 03:07:02 +0530

    Solved Navigation Problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,6 +211,7 @@ $(document).ready(function() {
   $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
+
       if (target.length) {
         var top_space = 0;
 
@@ -221,10 +222,14 @@ $(document).ready(function() {
             top_space = top_space - 20;
           }
         }
+        
+
+        var duration = Math.abs($(window).scrollTop()-target.offset().top)*0.5;
 
         $('html, body').animate({
           scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
+        }, duration,'swing');
+
 
         if ($(this).parents('.nav-menu').length) {
           $('.nav-menu .menu-active').removeClass('menu-active');
